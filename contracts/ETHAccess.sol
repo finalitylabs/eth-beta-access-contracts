c9712338e4823df145382a4f3f7d3b74fc84fa29
nginnever <ginneversource@gmail.com>
2018-07-27 19:26:39 -0700

    qrt beta purchase

--- contracts/ETHAccess.sol
+++ contracts/ETHAccess.sol
@@ -12,54 +12,87 @@ import "./lib/token/ERC721Token.sol";
 contract ETHAccess is Ownable, ERC721Token {
 
   // how long the sale is open for
-  uint256 public betaSaleTime = 0;
-  uint256 public numParticipants = 0;
-  uint256 public QRTprice;
+  uint256 public betaSaleEnd = 0;
+  uint256 public betaQRTLimit = 10; // 10 for testing, 5000 mainnet
+  uint256 public portalKittyLimit = 5; // 5 for testing, 500 mainnet
+  uint256 public totalPortalKitties = 0;
+  uint256 public QRTprice = 100 finney;
 
   CKInterface public ck;
 
   struct Participant {
     address party;
-    uint256 balance;
+    uint256 numPortalKitties;
   }
 
   mapping(address => Participant) public participants;
 
   modifier onlyBeta() {
-    require(now < betaSaleTime);
+    require(now < betaSaleEnd);
     _;
   }
 
-  constructor(uint256 betaSaleLength, address _ckAddress, string name, string symbol) public 
+  event QRTPurchase(
+    address indexed _from,
+    uint256 indexed _time,
+    uint256 indexed _tokenId
+  );
+
+  event KittiesPortal(
+    address indexed _from,
+    uint256 indexed _time
+  );
+
+  constructor(
+    uint256 betaSaleLength, 
+    address _ckAddress, 
+    string name, 
+    string symbol) 
+    public 
     ERC721Token(name, symbol)
   {
-    betaSaleTime = betaSaleLength;
+    betaSaleEnd = betaSaleLength;
     ck = CKInterface(_ckAddress);
   }
 
   function purchaseQRT() public payable {
-    require(msg.value == 100 finney);
-    require(now > betaSaleTime);
-    uint256 _tokenID = totalSupply().add(1);
-    super._mint(msg.sender, _tokenID);
+    require(msg.value == QRTprice);
+    require(now > betaSaleEnd);
 
+    uint256 _tokenID = totalSupply().add(1);
+
+    participants[msg.sender].party = msg.sender;
+
+    super._mint(msg.sender, _tokenID);
+    emit QRTPurchase(msg.sender, now, _tokenID);
   }
 
   function purchaseQRTbeta() onlyBeta public payable {
-    require(msg.value == 100 finney);
+    require(msg.value == QRTprice);
+    require(totalSupply() < betaQRTLimit);
+
     uint256 _tokenID = totalSupply().add(1);
+    participants[msg.sender].party = msg.sender;
+
     super._mint(msg.sender, _tokenID);
+    emit QRTPurchase(msg.sender, now, _tokenID);
   }
 
-  function portalCryptoKitties() onlyBeta public {
+  function portalCryptoKitties(uint256[10] ids) onlyBeta public {
     require(ck.balanceOf(msg.sender) >= 10);
+    require(totalPortalKitties <= portalKittyLimit);
+
+    // this assumes client calls an approval for each cryptokitty id
+    for(uint i=0; i<10; i++) {
+      ck.transferFrom(msg.sender, address(this), ids[i]);
+    }
+
+    participants[msg.sender].numPortalKitties = participants[msg.sender].numPortalKitties.add(1);
+    totalPortalKitties = totalPortalKitties.add(1);
+    emit KittiesPortal(msg.sender, now);
   }
 
-  function withdraw() onlyOwner {
-
-  }
-
-  function finalize() onlyOwner {
-    // transfer ownership of token from sale contract to msg.sender
+  function withdraw() onlyOwner public {
+    owner.transfer(address(this).balance);
   }
 }
--- test/saleContractTest.js
+++ test/saleContractTest.js
@@ -10,7 +10,7 @@ const web3latest = new Web3latest(new Web3latest.providers.HttpProvider("http://
 
 let ea
 let ckAddress = '0x0'
-let betaEndTime = '1'
+let betaEndTime = '1632743004'
 
 let partyA
 let partyB
@@ -27,6 +27,38 @@ contract('E.T.H. sale contract tests', function(accounts) {
   })
 
   it("set owner of token to sale contract", async () => {
-
+    let owner = await ea.owner()
+    let betaSaleEnd = await ea.betaSaleEnd()
+    assert(owner, partyA)
+    assert(betaSaleEnd, betaEndTime);
   })
+
+  it("can purchase QRT during beta", async () => {
+    let res = await ea.purchaseQRTbeta({from: partyB, value: web3latest.utils.toWei('0.1')})
+    let participant = await ea.participants(partyB)
+    let QRTsupply = await ea.totalSupply()
+    let ownerOf = await ea.ownerOf(QRTsupply)
+    assert(QRTsupply, '1')
+    assert(ownerOf, partyB)
+    assert(participant, partyB)
+  })
+
+  it("can not purchase with wrong ether amt", async () => {
+    await Utils.expectThrow(ea.purchaseQRTbeta({from: partyB, value: web3latest.utils.toWei('0.2')}))
+  }) 
+
+  it("can not purchase QRT mainsale during beta", async () => {
+    await Utils.expectThrow(ea.purchaseQRT({from: partyB, value: web3latest.utils.toWei('0.1')}))
+  }) 
+
+  it("can purchase up to QRT beta total", async () => {
+    for(var i=0; i<9; i++) {
+      let res = await ea.purchaseQRTbeta({from: partyA, value: web3latest.utils.toWei('0.1')})
+    }
+  })
+
+  it("can't purchase more than beta total before mainsale", async () => {
+    await Utils.expectThrow(ea.purchaseQRTbeta({from: partyB, value: web3latest.utils.toWei('0.1')}))
+  })
+
 })
