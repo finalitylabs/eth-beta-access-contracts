b4d28287488cfc4e3a8f9730e49ee2cdf0e03b0c
nginnever <ginneversource@gmail.com>
2018-07-30 01:19:08 -0700

    update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,6 @@
-// var EC = artifacts.require("./ECTools.sol");
-// var LC = artifacts.require("./LedgerChannel.sol");
+const ethAccess = artifacts.require('./ETHAccess.sol')
 
 module.exports = async function(deployer) {
-  // deployer.deploy(EC);
-  // deployer.link(EC, LC);
-  // deployer.deploy(LC);
-  // console.log(LC.address)
+    ea = await ethAccess.new('1632743004', '0x95ef2833688ee675dfc1350394619ae22b7667df', 'E.T.H. Quarter', 'QRTETH')
+    console.log(ea.address)
 };
--- test/saleContractTest.js
+++ test/saleContractTest.js
@@ -27,7 +27,7 @@ contract('E.T.H. sale contract tests', function(accounts) {
     console.log(partyA)
 
     ck = await kittyInterface.at(ckAddress)
-    //ea = await ethAccess.new(betaEndTime, ckAddress, 'E.T.H. Quarter', 'QRTETH')
+    //ea = await ethAccess.new(betaEndTime, ck.address, 'E.T.H. Quarter', 'QRTETH')
     ea = await ethAccess.at('0x7b19229b7845416b31710ccf39c1408e61fc0e77')
   })
 
