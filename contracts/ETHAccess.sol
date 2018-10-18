d9634a7622c049e2698ff9a2f5b53069b4f54ce8
Jasper de Gooijer <j.degooijer@triple-it.nl>
2018-10-18 16:10:37 +0200

    just a small comment of something we probably are going to want to add if we're going to have a time limit on the contract which would make sense

--- contracts/ETHAccess.sol
+++ contracts/ETHAccess.sol
@@ -8,91 +8,95 @@ pragma solidity ^0.4.23;
 import "./lib/ownership/Ownable.sol";
 import "./lib/token/CKInterface.sol";
 import "./lib/token/ERC721Token.sol";
+import "./lib/math/SafeMath.sol";
 
 contract ETHAccess is Ownable, ERC721Token {
+    
+    using SafeMath for uint;
+    // how long the sale is open for
+    uint256 public betaSaleEnd = 0;
+    uint256 public betaQRTLimit = 10; // 10 for testing, 5000 mainnet
+    uint256 public portalKittyLimit = 5; // 5 for testing, 500 mainnet
+    uint256 public totalPortalKitties = 0;
+    uint256 public QRTprice = 100 finney;
 
-  // how long the sale is open for
-  uint256 public betaSaleEnd = 0;
-  uint256 public betaQRTLimit = 10; // 10 for testing, 5000 mainnet
-  uint256 public portalKittyLimit = 5; // 5 for testing, 500 mainnet
-  uint256 public totalPortalKitties = 0;
-  uint256 public QRTprice = 100 finney;
+    CKInterface public ck;
 
-  CKInterface public ck;
-
-  struct Participant {
-    address party;
-    uint256 numPortalKitties;
-  }
+    struct Participant {
+        address party;
+        uint256 numPortalKitties;
+    }
 
   // we can use this mapping to allow kitty depositers to claim an E.T.H. fighter NFT in the future
-  mapping(address => Participant) public participants;
+    mapping(address => Participant) public participants;
 
-  modifier onlyBeta() {
-    require(now < betaSaleEnd);
-    _;
-  }
+    modifier onlyBeta() {
+        require(now < betaSaleEnd);
+        _;
+    }
 
-  event QRTPurchase(
-    address indexed _from,
-    uint256 indexed _time,
-    uint256 indexed _tokenId
-  );
+    event QRTPurchase(
+        address indexed _from,
+        uint256 indexed _time,
+        uint256 indexed _tokenId
+    );
 
-  event KittiesPortal(
-    address indexed _from,
-    uint256 indexed _time
-  );
+    event KittiesPortal(
+        address indexed _from,
+        uint256 indexed _time
+    );
 
-  constructor(
-    uint256 betaSaleLength, 
-    address _ckAddress, 
-    string name, 
-    string symbol) 
-    public 
-    ERC721Token(name, symbol)
-  {
-    betaSaleEnd = betaSaleLength;
-    ck = CKInterface(_ckAddress);
-    super._mint(msg.sender, 0);
-  }
+    constructor(
+        uint256 betaSaleLength, 
+        address _ckAddress, 
+        string name, 
+        string symbol) 
+        public 
+        ERC721Token(name, symbol)
+    {
+        betaSaleEnd = betaSaleLength;
+        ck = CKInterface(_ckAddress);
+        super._mint(msg.sender, 0);
+    }
 
-  function purchaseQRT() public payable {
-    require(msg.value == QRTprice);
-    require(now > betaSaleEnd);
+    function purchaseQRT() public payable {
+        require(msg.value == QRTprice);
+        require(now > betaSaleEnd);
 
-    uint256 _tokenID = totalSupply().add(1);
+        uint256 _tokenID = totalSupply().add(1);
 
-    participants[msg.sender].party = msg.sender;
+        participants[msg.sender].party = msg.sender;
 
-    super._mint(msg.sender, _tokenID);
-    emit QRTPurchase(msg.sender, now, _tokenID);
-  }
+        super._mint(msg.sender, _tokenID);
+        emit QRTPurchase(msg.sender, now, _tokenID);
+    }
 
-  function purchaseQRTbeta() onlyBeta public payable {
-    require(msg.value == QRTprice);
-    require(totalSupply() < betaQRTLimit);
+    function purchaseQRTbeta() onlyBeta public payable {
+        require(msg.value == QRTprice);
+        require(totalSupply() < betaQRTLimit);
+        // TODO: uncomment in the future when a sale time is decided on.
+        //require(now < betaSaleEnd); 
 
-    uint256 _tokenID = totalSupply().add(1);
-    participants[msg.sender].party = msg.sender;
+        uint256 _tokenID = totalSupply().add(1);
+        participants[msg.sender].party = msg.sender;
 
-    super._mint(msg.sender, _tokenID);
-    emit QRTPurchase(msg.sender, now, _tokenID);
-  }
+        super._mint(msg.sender, _tokenID);
+        emit QRTPurchase(msg.sender, now, _tokenID);
+    }
 
-  function portalKitty(uint256 id) onlyBeta public {
-    require(ck.ownerOf(id) == msg.sender);
-    require(totalPortalKitties <= portalKittyLimit);
+    function portalKitty(uint256 id) onlyBeta public {
+        require(ck.ownerOf(id) == msg.sender);
+        require(totalPortalKitties <= portalKittyLimit);
 
-    // this assumes client calls an approval for each cryptokitty id
-    ck.transferFrom(msg.sender, address(this), id);
+        // this assumes client calls an approval for each cryptokitty id
+        ck.transferFrom(msg.sender, address(this), id);
 
-    participants[msg.sender].numPortalKitties = participants[msg.sender].numPortalKitties.add(1);
-    totalPortalKitties = totalPortalKitties.add(1);
-    emit KittiesPortal(msg.sender, now);
-  }
+        participants[msg.sender].numPortalKitties = participants[msg.sender].numPortalKitties.add(1);
+        totalPortalKitties = totalPortalKitties.add(1);
+        emit KittiesPortal(msg.sender, now);
+    }
 
-  function withdraw() onlyOwner public {
-    owner.transfer(address(this).balance);
-  }
+    function withdraw() onlyOwner public {
+        owner.transfer(address(this).balance);
+    }
 }
